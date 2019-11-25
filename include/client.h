448267c21d72ff6bb7b01a5c959018f20bcf25a4
Tóth Ferenc <tothfo@gmail.com>
2019-11-25 22:14:27 +0100

    szerver-kliens

---
+++ include/client.h
@@ -0,0 +1,16 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+
+class client
+{
+    public:
+        client();
+        virtual ~client();
+
+    protected:
+
+    private:
+};
+
+#endif // CLIENT_H
---
+++ include/server.h
@@ -0,0 +1,16 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+
+class server
+{
+    public:
+        server();
+        virtual ~server();
+
+    protected:
+
+    private:
+};
+
+#endif // SERVER_H
